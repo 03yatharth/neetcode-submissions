7f0cce3e50ebf90571d0740ea8f6d7b1a5a49853
03yatharth <146108197+03yatharth@users.noreply.github.com>
2026-09-09 20:35:21 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n<10){
+            if(n==1 || n==7)return true;
+            return false;
+        }
+        int sum=0;
+        while(n){
+            int bit = n%10;
+            sum += (bit*bit);
+            n/=10;
+        }
+        return isHappy(sum);
+    }
+};
